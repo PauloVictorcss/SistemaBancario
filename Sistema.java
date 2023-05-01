3c8f30432c76a14aa82442d4e482f37d7a692d82
Paulo Victor <123316548+PauloVictorcss@users.noreply.github.com>
2023-05-01 14:41:27 -0300

    editando

--- Sistema.java
+++ Sistema.java
@@ -23,33 +23,33 @@ public class SistemaBancário {
 				       4- Sair
 		  	           """;
 		int num;
-    do {	
-        System.out.println(opção);
-        num = leitura.nextInt();
-
-        if(num == 1) {
-          System.out.println("Saldo: " + saldoInicial);
-        }else if(num == 2) {
-            System.out.println("Informe o valor que deseja receber: ");
-            double receber = leitura.nextDouble();
-            saldoInicial += receber;
-        }else if(num == 3) {
-          System.out.println("Informe um valor que deseja transferir: ");
-          double transferir = leitura.nextDouble();
-          if(saldoInicial < transferir) {
-            System.out.println("Não suficiente para realizar trasnferencia");;
-          }else {
-            saldoInicial -= transferir;
-          }
-        }else if(num == 4) {
-          System.out.println("*****************************");
-          System.out.println("Operação finalizada");
-          System.out.println("Nome: " + nome);
-          System.out.println("Tipo de conta: " + tipoConta);
-          System.out.println("Saldo: " + saldoInicial);
-          System.out.println("******************************");
-			 }
+                do {	
+			System.out.println(opção);
+			num = leitura.nextInt();
+			
+			if(num == 1) {
+				System.out.println("Saldo: " + saldoInicial);
+			}else if(num == 2) {
+			    System.out.println("Informe o valor que deseja receber: ");
+			    double receber = leitura.nextDouble();
+			    saldoInicial += receber;
+			}else if(num == 3) {
+				System.out.println("Informe um valor que deseja transferir: ");
+				double transferir = leitura.nextDouble();
+				if(saldoInicial < transferir) {
+					System.out.println("Não suficiente para realizar trasnferencia");;
+				}else {
+					saldoInicial -= transferir;
+				}
+			}else if(num == 4) {
+				System.out.println("*****************************");
+				System.out.println("Operação finalizada");
+				System.out.println("Nome: " + nome);
+				System.out.println("Tipo de conta: " + tipoConta);
+				System.out.println("Saldo: " + saldoInicial);
+				System.out.println("******************************");
+			}
 	   }while(num != 4);
 
+     }
    }
-}
